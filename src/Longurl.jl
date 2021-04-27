d23d01ce2e67d1dada0da3e99e7277c5af67ac6b
newzealandpaul <nzpaul@nospam.example.com>
2021-04-27 13:30:45 +1200

    Use request data rather than verbose, change status to Int64

--- src/Longurl.jl
+++ src/Longurl.jl
@@ -8,7 +8,7 @@ using HTTP
 """
 struct Urls
     expanded_url::Vector{Union{String, Nothing}}
-    status_code::Vector{Union{String, Nothing}}
+    status_code::Vector{Union{Int64, Nothing}}
 end
 
 """
@@ -24,42 +24,39 @@ Takes a list of short urls and exands them into their long form
 """
 function expand_urls(urls_to_expand::A, seconds::N=2) where {A<:Union{String,Vector{String}}, N <: Number} 
 
-    original_stdout = stdout
-    original_error = stderr
-
     short_urls = Vector{Union{String, Nothing}}(nothing, length(urls_to_expand))
     expanded_urls = Vector{Union{String, Nothing}}(nothing, length(urls_to_expand))
-    status_codes = Vector{Union{String, Nothing}}(nothing, length(urls_to_expand))
+    status_codes = Vector{Union{Int64, Nothing}}(nothing, length(urls_to_expand))
 
     i = 0
     for url in urls_to_expand
         i += 1
-        last_head = nothing
+        last_target = nothing
         last_host = nothing
         last_code = nothing
-        (rd, wr) = redirect_stdout()
 
         try
-            HTTP.head(url, readtimeout=seconds, verbose=2, retry=false)
-        catch e
-
-        finally
-            redirect_stdout(original_stdout)
-            close(wr)  
-            for line in readlines(rd)
-                if occursin("HEAD", line)
-                    last_head = split(line, " ")[2]
-                end
-                if occursin("Host", line)
-                    last_host = split(line, " ")[2]
-                end
-                if occursin("HTTP/1.1 ", line)
-                    last_code = split(line, " ")[2]
+            res = HTTP.get(url, readtimeout=seconds, retry=false, redirect = true, status_exception = false, verbose = 2)
+            req = res.request
+            last_code = res.status
+            print(last_code)
+            for h in req.headers
+                if h[1] == "Host"
+                    last_host = h[2]
                 end
             end
+            last_target = req.target
+               
+        catch e
+            print(e)
+        finally
             short_urls[i] = url
             status_codes[i] = last_code
-            expanded_urls[i] = last_host * last_head
+            if last_host != nothing || last_target != nothing
+                expanded_urls[i] = last_host * last_target
+            else
+                expanded_urls[i] = nothing
+            end
         end
     end 
     
@@ -70,3 +67,5 @@ function expand_urls(urls_to_expand::A, seconds::N=2) where {A<:Union{String,Vec
 end
 
 end # module
+
+
