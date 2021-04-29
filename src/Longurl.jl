1c32c6b599d2ba5eb28c72373736e690ab0634af
Jamie <jamierayhutchings@gmail.com>
2021-04-29 17:58:48 +1200

    Refactor

--- src/Longurl.jl
+++ src/Longurl.jl
@@ -3,74 +3,92 @@ export expand_urls
 
 using HTTP
 
-"""
-    Urls(expanded_url, status_code)
-"""
-struct Urls
-    expanded_url::Vector{Union{String, Nothing}}
-    status_code::Vector{Union{String, Nothing}}
-end
 
 """
-Takes a list of short urls and expands them into their long form
+    Url(expanded_url, status_code)
+"""
+struct Url
+    expanded_url::Union{String, Nothing}
+    status_code::Union{String, Nothing}
+end
+
+
+"""
+Takes a short url and expands it into their long form
 
 ...
 # Arguments
-- `urls_to_expand::Array`: the list of short urls
+- `url_to_expand::String`: the short url
 - `seconds::Int64`: the timeout in seconds
 # Returns
-- `Urls`: Struct containing properties expanded_url and status_code
+- `Url`: Struct containing properties expanded_url and status_code
 ...
 """
-function expand_urls(urls_to_expand::A, seconds::N=2) where {A<:Union{String,Vector{String}}, N <: Number} 
-
-    if urls_to_expand isa String
-        urls_to_expand = [urls_to_expand]
-    end
+function expand_url(url_to_expand::A, seconds::N=2) where {A<:String, N <: Number} 
 
     original_stdout = stdout
     original_error = stderr
 
-    short_urls = Vector{Union{String, Nothing}}(nothing, length(urls_to_expand))
-    expanded_urls = Vector{Union{String, Nothing}}(nothing, length(urls_to_expand))
-    status_codes = Vector{Union{String, Nothing}}(nothing, length(urls_to_expand))
+    short_url = Union{String, Nothing}
+    expanded_url = Union{String, Nothing}
+    status_code = Union{String, Nothing}
+
+    last_head = nothing
+    last_host = nothing
+    last_code = nothing
+    (rd, wr) = redirect_stdout()
+
+    try
+        HTTP.head(url_to_expand, readtimeout=seconds, verbose=2, retry=false)
+    catch e
+
+    finally
+        redirect_stdout(original_stdout)
+        close(wr)  
+        for line in readlines(rd)
+            if occursin("HEAD", line)
+                last_head = split(line, " ")[2]
+            end
+            if occursin("Host", line)
+                last_host = split(line, " ")[2]
+            end
+            if occursin("HTTP/1.1 ", line)
+                last_code = split(line, " ")[2]
+            end
+        end
+        short_url = url_to_expand
+        status_code = last_code
+        expanded_url = last_host * last_head
+    end
+    
+    long_url = Url(expanded_url, status_code)
+
+    return long_url
+end
+
+
+"""
+Takes a vector of short urls and expands them into their long form
+
+...
+# Arguments
+- `urls_to_expand::Vector{String}`: the short urls
+- `seconds::Int64`: the timeout in seconds
+# Returns
+- `Url`: Struct containing properties expanded_url and status_code
+...
+"""
+function expand_urls(urls_to_expand::A, seconds::N=2) where {A<:Vector{String}, N <: Number} 
+
+    long_urls = Vector{Url}(undef, length(urls_to_expand))
 
     i = 0
     for url in urls_to_expand
         i += 1
-        last_head = nothing
-        last_host = nothing
-        last_code = nothing
-        (rd, wr) = redirect_stdout()
-
-        try
-            HTTP.head(url, readtimeout=seconds, verbose=2, retry=false)
-        catch e
-
-        finally
-            redirect_stdout(original_stdout)
-            close(wr)  
-            for line in readlines(rd)
-                if occursin("HEAD", line)
-                    last_head = split(line, " ")[2]
-                end
-                if occursin("Host", line)
-                    last_host = split(line, " ")[2]
-                end
-                if occursin("HTTP/1.1 ", line)
-                    last_code = split(line, " ")[2]
-                end
-            end
-            short_urls[i] = url
-            status_codes[i] = last_code
-            expanded_urls[i] = last_host * last_head
-        end
-    end 
-    
-    long_urls = Urls(expanded_urls, status_codes)
+        long_urls[i] = expand_url(url)
+    end
 
     return long_urls
-
 end
 
-end # module
+end
